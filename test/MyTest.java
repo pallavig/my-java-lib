9c9da0037c8737a4449349a74121a9dcefa2e579
Pallavi <pallavig@thoughtworks.com>
2014-01-25 16:03:26 +0530

    two more tests

--- test/MyTest.java
+++ test/MyTest.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 public class MyTest {
     @Test
     public void test() {
-        assert(1==1);   
+        assert(1==1);
+        assert(10==10);
+        assert(!("shital".equals("pallavi")));
     }
 }
